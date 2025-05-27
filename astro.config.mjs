24c255cabd141662c285de352bf9bfdda74fb9e9
pilosophos <pilosophos@gmail.com>
2025-05-26 21:11:09 -0700

    commit deploy files

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pilosophos.com',
+  site: 'https://test.pilosophos.com',
   vite: {
     plugins: [tailwindcss()]
   }
