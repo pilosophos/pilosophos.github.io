46e61f6f20c6255fba2aa80d8fb3cc5c454a8238
pilosophos <pilosophos@gmail.com>
2025-05-26 22:25:36 -0700

    change site config for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://test.pilosophos.com',
+  site: 'https://pilosophos.com',
   vite: {
     plugins: [tailwindcss()]
   }
