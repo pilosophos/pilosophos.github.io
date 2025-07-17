a7b7cba64572142dafb32a67d44f19e862451720
pilosophos <pilosophos@gmail.com>
2025-07-16 19:04:23 -0700

    generate individual comic pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -32,5 +32,19 @@ const art = defineCollection({
     }),
 });
 
+const comic = defineCollection({
+  loader: glob({
+    pattern: '**/*.md',
+    base: './src/comic',
+  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      cover: image(),
+      date: z.optional(z.date()),
+      mouseover: z.optional(z.string())
+    }),
+});
+
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { writing, art };
+export const collections = { writing, art, comic };
