f03eefcfb05c461ab43d8cdcb0b0450e65b4e705
pilosophos <pilosophos@gmail.com>
2026-03-01 16:29:19 -0800

    add flowchart

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import mermaid from 'astro-mermaid';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -13,5 +14,20 @@ export default defineConfig({
   build: {
     format: 'file',
   },
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    mermaid({
+      theme: 'base',
+      autoTheme: false,
+      mermaidConfig: {
+        fontFamily: "var(--font-sans)",
+        themeVariables: {
+          primaryColor: '#9eecec07',
+          primaryTextColor: '#9eecec',
+          primaryBorderColor: '#9eecec',
+          lineColor: '#fecb00',
+        }
+      }
+    })
+  ],
 });
